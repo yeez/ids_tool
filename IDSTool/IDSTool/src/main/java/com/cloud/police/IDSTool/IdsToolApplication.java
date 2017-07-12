3a7350a6dc23818a91e8f16e614b65d6b8ebb2b1
ZZ00ZZ <abdulhadi.zaid@gmail.com>
2017-07-12 19:05:05 -0400

    initial microservice structure

---
+++ IDSTool/IDSTool/src/main/java/com/cloud/police/IDSTool/IdsToolApplication.java
@@ -0,0 +1,12 @@
+package com.cloud.police.IDSTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdsToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdsToolApplication.class, args);
+	}
+}
---
+++ IDSTool/IDSTool/src/test/java/com/cloud/police/IDSTool/IdsToolApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cloud.police.IDSTool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IdsToolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
